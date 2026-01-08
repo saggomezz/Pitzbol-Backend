fdc518b0142ab47b59b0c9415811d92b2b1a7a39
PilarMora <pilarmorag2004@hotmail.com>
2026-01-08 15:27:41 -0600

    Implementación de flujo para guia y admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -3,10 +3,7 @@ import * as adminController from '../controllers/admin.controller';
 
 const router = Router();
 
-// Ruta para obtener la lista de guías en revisión
 router.get('/solicitudes-pendientes', adminController.getSolicitudesPendientes);
-
-// Ruta para aprobar o rechazar a un guía
 router.post('/gestionar-guia', adminController.gestionarSolicitudGuia);
 
 export default router;
