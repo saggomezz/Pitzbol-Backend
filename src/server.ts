33168897dbd823cd178191634eba9d74e4241928
Manuel Mendoza <a22310349@ceti.mx>
2026-01-08 19:27:55 -0600

    Cambios en Stipe

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/auth.routes';
-import guideRoutes from './routes/guide.routes';
+
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes";
+import guideRoutes from "./routes/guide.routes";
 import businessRoutes from "./routes/business.routes";
+import paymentRoutes from "./routes/payment.routes"; 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,25 +15,25 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-// (Middleware)
+// Middleware de logging
 app.use((req, res, next) => {
-    console.log(`Petición recibida: [${req.method}] ${req.url}`);
-    next();
+  console.log(`Petición recibida: [${req.method}] ${req.url}`);
+  next();
 });
-// ------------------------------------------------
 
-app.use('/api/auth', authRoutes);
-app.use('/api/guides', guideRoutes);
+// Rutas
+app.use("/api/auth", authRoutes);
+app.use("/api/guides", guideRoutes);
 app.use("/api/business", businessRoutes);
+app.use("/api/payments", paymentRoutes); 
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>El Backend sí funcionaa</h1>
-        <p>El servidor está corriendo correctamente</p>
-        <p>Usa los endpoints en <code>/api/auth/login</code> o <code>/api/auth/register</code></p>
-    `);
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>El Backend sí funciona</h1>
+    <p>El servidor está corriendo correctamente</p>
+  `);
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
